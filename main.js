459c068d3eadec2baf95a247727e79dcb182cbf9
Talpedreros <tomas.larenas@virginiogomez.cl>
2025-09-11 20:17:20 -0300

    modificado

--- main.js
+++ main.js
@@ -1,12 +1,31 @@
-const validar = () => {
+function validar() {
+  const nombre = document.getElementById("nombre").value.trim();
+  const email = document.getElementById("email").value.trim();
+  const telefono = document.getElementById("telefono").value.trim();
+  const estadoCivil = document.getElementById("estadoCivil").value;
+  const comentario = document.getElementById("comentario").value.trim();
 
-    document.querySelectorAll('.form-control, .form-select').forEach((item) =>{ 
+  if (!nombre || !email || !telefono || !estadoCivil || !comentario) {
+    alert("Todos los campos son obligatorios");
+    return;
+  }
+  const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!regexEmail.test(email)) {
+    alert("El email no es válido.");
+    return;
+  }
+  const regexTel = /^[0-9]{8,15}$/;
+  if (!regexTel.test(telefono)) {
+    alert("El teléfono debe contener solo números (mínimo 8 dígitos).");
+    return;
+  }
 
-        if(item.value.trim()==''){
-            item.classList.add('is-invalid')
-        }else{
-            item.classList.add('is-valid')
-        }
+  alert("Formulario enviado correctamente.");
+  limpiar();
+}
 
-    })
+
+
+function limpiar() {
+  document.getElementById("formGamer").reset();
 }
